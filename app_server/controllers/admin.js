7ed74d71ab9c005811ae89cdb0cfec97647fc36c
Chris Davies <chrisd27@hotmail.com>
2016-09-20 20:39:25 +0100

    Completed Player, Game and Rule JS object Structure

--- app_server/controllers/admin.js
+++ app_server/controllers/admin.js
@@ -2,52 +2,83 @@
 module.exports.teamSummary = function(req, res){
   res.render('admin/teamSummary', { 
   	title: 'Atholl 1965 Summary',
-  	players: {
-  		details: [{
-  		  id: '1', 
-  		  name: 'Chris Davies', 
-  		  fantasy: '9', 
-  		  goals: '100', 
-  		  assists: '99'
-  		},{ 
-  		  id: '2', 
-  	    name: 'Rich Burton',
-  	    fantasy: '0', 
-  	    goals: '-12', 
-  	    assists: '-13'
-  	    }]
-  	},
-    games: {
-    	details: [{
-  		    id: '1', 
-  		    opposition: 'Wales', 
-  		    score: '3-0', 
-  		    date: '21/04/2016', 
-  		    MOTM: 'Chris Davies'
-  	    },{
-  	    	id: '2', 
-  		    opposition: 'Jonny Spurving', 
-  		    score: '2-2', 
-  		    date: '10/05/2016', 
-  		    MOTM: 'SAB'
-  		}]
-  	},
-    rules: {
-      details: [{
-          id: '1', 
-          rule: 'Goal', 
-          points: '5', 
-          position: 'Defender',
-          custom: 'No'
-        },{
-          id: '2', 
-          rule: 'Assist', 
-          points: '3', 
-          position: 'Defender',
-          custom: 'No'
-      }]
-    }
-  });
+  	players: [{
+		id: '1', 
+		details: {
+			name: 'Chris Davies'
+		},
+		stats: {
+			goals: '12',
+			assists: '10',
+			redCards: '2',
+			yellowCards: '1',
+			motm: '0'
+		},
+		points: {
+			goals: '100',
+			assists: '120',
+			redCards: '30',
+			yellowCards: '60',
+			motm: '2',
+			total: '1000'
+		},
+		games: [{
+			id: '1'
+		}, {
+			id: '2'
+		}],
+	},{
+  		id: '2', 
+  		details: {
+  			name: 'Rich Burton'
+  		},
+  		stats: {
+  			goals: '1',
+  			assists: '3',
+  			redCards: '10',
+  			yellowCards: '29',
+  			motm: '0'
+  		},
+  		points: {
+  			goals: '10',
+  			assists: '12',
+  			redCards: '344',
+  			yellowCards: '603',
+  			motm: '0'
+  		},
+  		games: [{
+  			id: '1'
+  		}, {
+  			id: '2'
+  		}],
+  	}],
+    games: [{
+  	    id: '1', 
+  	    opposition: 'Wales', 
+  	    score: '3-0', 
+  	    date: '21/04/2016', 
+  	    MOTM: 'Chris Davies'
+  	},{
+  	   	id: '2', 
+  	    opposition: 'Jonny Spurving', 
+  	    score: '2-2', 
+  	    date: '10/05/2016', 
+  	    MOTM: 'SAB'
+  	}],
+  	rules:[{
+        id: '1', 
+        rule: 'Goal', 
+        points: '5', 
+        position: 'Defender',
+        custom: 'No'
+    },{
+        id: '2', 
+        rule: 'Assist', 
+        points: '3', 
+        position: 'Defender',
+        custom: 'No'
+     }],
+   });
 };
 
 /* Get 'add rules' page */
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,81 @@
 module.exports.main = function(req, res){
-  res.render('index',  {title: 'Home page' });
-};	
+  res.render('index', { 
+  	title: 'Atholl 1965 Summary',
+  	players: [{
+		id: '1', 
+		details: {
+			name: 'Chris Davies'
+		},
+		stats: {
+			goals: '12',
+			assists: '10',
+			redCards: '2',
+			yellowCards: '1',
+			motm: '0'
+		},
+		points: {
+			goals: '100',
+			assists: '120',
+			redCards: '30',
+			yellowCards: '60',
+			motm: '2',
+			total: '1000'
+		},
+		games: [{
+			id: '1'
+		}, {
+			id: '2'
+		}],
+	},{
+  		id: '2', 
+  		details: {
+  			name: 'Rich Burton'
+  		},
+  		stats: {
+  			goals: '1',
+  			assists: '3',
+  			redCards: '10',
+  			yellowCards: '29',
+  			motm: '0'
+  		},
+  		points: {
+  			goals: '10',
+  			assists: '12',
+  			redCards: '344',
+  			yellowCards: '603',
+  			motm: '0'
+  		},
+  		games: [{
+  			id: '1'
+  		}, {
+  			id: '2'
+  		}],
+  	}],
+    games: [{
+  	    id: '1', 
+  	    opposition: 'Wales', 
+  	    score: '3-0', 
+  	    date: '21/04/2016', 
+  	    MOTM: 'Chris Davies'
+  	},{
+  	   	id: '2', 
+  	    opposition: 'Jonny Spurving', 
+  	    score: '2-2', 
+  	    date: '10/05/2016', 
+  	    MOTM: 'SAB'
+  	}],
+  	rules:[{
+        id: '1', 
+        rule: 'Goal', 
+        points: '5', 
+        position: 'Defender',
+        custom: 'No'
+    },{
+        id: '2', 
+        rule: 'Assist', 
+        points: '3', 
+        position: 'Defender',
+        custom: 'No'
+     }],
+   });
+};
