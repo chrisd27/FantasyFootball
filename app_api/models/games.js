8fce25135c81af2d6eb3d9e91d236f154cba6e31
chrisd27 <chris@chrisdavies.ninja>
2016-10-13 14:25:01 +0100

    Added api requests in the server-side controllers for the main page

--- app_api/models/games.js
+++ app_api/models/games.js
@@ -2,9 +2,19 @@ var mongoose = require('mongoose');
 
 var gamesSchema = new mongoose.Schema({
     opposition: {type: String, required: true}, 
-    score: {type: String, required: true}, 
+    oppositionScore: {type: Number, required: true}, 
+    accountTeamScore: {type: Number, required: true}, 
     date: {type: String, required: true}, 
-    result: {type: String, required: true}
+    players: [{
+    	playerId: String,
+    	stats: [{
+    		ruleId: String,
+    		ruleInstances: Number,
+    		ruleTotal: Number,
+    		ruleCalcualtedTotal: Number
+    	}],
+    }],
+
 });
 
 mongoose.model('Game', gamesSchema);
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,212 +1,106 @@
-module.exports.main = function(req, res){
+var request = require('request'),
+apiOptions = {
+  server: "http://localhost:3000"
+},
+responseBody = {};
+
+// TODO - Add production URL Heroku here
+if(process.env.NODE_ENV === "production"){
+  apiOptions.server = "http://localhost:3000";
+}
+
+var renderMainpage = function(req, res, respBody){
+
   res.render('index', { 
     title: 'Atholl 1965 Summary',
-    players: [{
-      id: '1', 
-      details: {
-        name: 'Chris Davies'
-      },
-     stats: [{
-       gameId: '4',
-       rules:[{
-          id: '1',
-          value: '2'
-        },{
-          id: '2',
-          value: '2'
-        },{
-          id: '3',
-          value: '2'
-        },{
-          id: '4',
-          value: '5'
-        },{
-          id: '5',
-          value: '2'
-        },{
-          id: '6',
-          value: '2'
-        },{
-          id: '7',
-          value: '2'
-        },{
-          id: '8',
-          value: '0'
-        }],
-        position: 'D'
-      },{
-       gameId: '1',
-         rules:[{
-          id: '1',
-          value: '2'
-        },{
-          id: '2',
-          value: '2'
-        },{
-          id: '3',
-          value: '2'
-        },{
-          id: '4',
-          value: '5'
-        },{
-          id: '5',
-          value: '2'
-        },{
-          id: '6',
-          value: '2'
-        },{
-          id: '7',
-          value: '2'
-        },{
-          id: '8',
-          value: '0'
-        }],
-        position: 'D'
-      }],
-      totals: {
-        goals: '10',
-        goalPoints: '2',
-        assists: '3',
-        assistPoints: '12',
-        redCards: '344',
-        yellowCards: '603',
-        motm: '0',
-        fantasyTotal: '1222'
-      },
-    },{
-      id: '2', 
-      details: {
-        name: 'Rich Burton'
-      },
-      stats: [{
-        gameId: '1',
-        rules:[{
-          id: '1',
-          value: '2'
-        },{
-          id: '2',
-          value: '2'
-        },{
-          id: '3',
-          value: '2'
-        },{
-          id: '4',
-          value: '5'
-        },{
-          id: '5',
-          value: '2'
-        },{
-          id: '6',
-          value: '2'
-        },{
-          id: '7',
-          value: '2'
-        },{
-          id: '8',
-          value: '0'
-        }],
-        position: 'D'
-      },{
-        gameId: '2',
-        rules:[{
-          id: '1',
-          value: '2'
-        },{
-          id: '2',
-          value: '2'
-        },{
-          id: '3',
-          value: '2'
-        },{
-          id: '4',
-          value: '5'
-        },{
-          id: '5',
-          value: '2'
-        },{
-          id: '6',
-          value: '2'
-        },{
-          id: '7',
-          value: '2'
-        },{
-          id: '8',
-          value: '0'
-        }],
-        position: 'D'
-      }],
-      totals: {
-        goals: '10',
-        goalPoints: '2',
-        assists: '3',
-        assistPoints: '12',
-        redCards: '344',
-        yellowCards: '603',
-        motm: '0',
-        fantasyTotal: '1222'
-      },
-    }],
-    games: [{
-        id: '1', 
-        opposition: 'Wales', 
-        score: '3-0', 
-        date: '21/04/2016', 
-        result: 'W'
-    },{
-        id: '2', 
-        opposition: 'Jonny Spurving', 
-        score: '2-2', 
-        date: '10/05/2016', 
-        result: 'D'
-    }],
-    rules:[{
-        id: '1', 
-        rule: 'Goal', 
-        points: '5', 
-        position: 'D',
-        custom: 'No'
-    },{
-        id: '2', 
-        rule: 'Goal pts', 
-        points: '3', 
-        position: 'D',
-        custom: 'No'
-     },{  
-        id: '3', 
-        rule: 'Assists', 
-        points: '3', 
-        position: 'Defender',
-        custom: 'No'
-     },{
-        id: '4', 
-        rule: 'Assist pts', 
-        points: '3', 
-        position: 'Defender',
-        custom: 'No'
-     },{
-        id: '5', 
-        rule: 'Red', 
-        points: '3', 
-        position: 'Defender',
-        custom: 'No'
-     },{
-        id: '6', 
-        rule: 'Yellow', 
-        points: '3', 
-        position: 'Defender',
-        custom: 'No'
-     },{
-        id: '7', 
-        rule: 'Card pts', 
-        points: '3', 
-        position: 'Defender',
-        custom: 'No'
-     },{
-        id: '8', 
-        rule: 'MOTM', 
-        points: '3', 
-        position: 'Defender',
-        custom: 'No'
-     }],
+    games: respBody.games,
+    players: respBody.players,
+    rules: respBody.rules
    });
+}
+
+
+module.exports.main = function(req, res){
+  var requestOptions = {}, path = {}, respCount = 0;
+  
+  // Games request
+  path.games = "/api/games";
+  requestOptions.getGames = {
+    url: apiOptions.server + path.games,
+    method: "GET",
+    json: {},
+    qs: {
+      offset: 20
+    }
+  }
+
+  request(requestOptions.getGames, function(err, response, body){
+    respCount++;
+    if(err){
+       return console.log(err);
+    } else if(response.statusCode === 200){
+        responseBody.games = body;
+        if(respCount === 3){
+          renderMainpage(req, res, responseBody);
+        }
+    } else {
+        console.log(response.statusCode);
+    }
+    
+  });
+
+  // Players request
+  path.players = "/api/players";
+  requestOptions.getPlayers = {
+    url: apiOptions.server + path.players,
+    method: "GET",
+    json: {},
+    qs: {
+      offset: 20
+    }
+  }
+
+  request(requestOptions.getPlayers, function(err, response, body){
+    respCount++;
+    if(err){
+        return console.log(err);
+    } else if(response.statusCode === 200){
+        responseBody.players = body;
+        if(respCount === 3){
+          renderMainpage(req, res, responseBody);
+        }
+    } else {
+        console.log(response.statusCode);
+    }
+  });
+
+  // Rules request
+  path.rules = "/api/rules";
+  requestOptions.getRules = {
+    url: apiOptions.server + path.rules,
+    method: "GET",
+    json: {},
+    qs: {
+      offset: 20
+    }
+  }
+
+  request(requestOptions.getRules, function(err, response, body){
+    respCount++;
+    if(err){
+        return console.log(err);
+    } else if(response.statusCode === 200){
+      if(body){
+        responseBody.rules = body;
+        if(respCount === 3){
+          renderMainpage(req, res, responseBody);
+        }
+      } else {
+        responseBody.rules = "No rules set";
+      }
+    } else {
+        console.log(response.statusCode);
+    }
+  });
+
 };
